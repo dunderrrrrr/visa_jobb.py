d9068356a515135a0a92ca8847b683c982f08f39
dunderrrrrr <emil.bjorkroth@gmail.com>
2018-02-12 18:30:47 +0100

    settings

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ settings = {
 
 options = {
     parser.add_option("-l", "--lan", dest="lan",help="Visa antal lediga jobb per län. Använd \"-l all\" för att visa alla län."),
-    parser.add_option("-k", "--kommun", dest="kom",help="Visa lediga jobb per kommun. Använd \"-k all\" för att visa alla län."),
+    parser.add_option("-k", "--kommun", dest="kom",help="Visa lediga jobb per kommun. Använd \"-k all\" för att visa alla kommuner."),
     parser.add_option("-t", "--total", dest="total",help="Visa totalt antal platsannonser samt lediga jobb.", action="store_true")
 }
 (options, args) = parser.parse_args()
